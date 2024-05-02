ce9fe4307dd72890407cca50f3b80368c07a6077
Felipe Garcia <felipegarcia@icop.edu.ar>
2024-05-02 17:22:03 -0300

    tercer ejercicio

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una frase: ");
+        String frase = scanner.nextLine();
+
+        System.out.println("Tu frase es: " + frase);
+        System.out.println("Tu frase en minúsculas: " + frase.toLowerCase());
+        System.out.println("Tu frase en mayúsculas: " + frase.toUpperCase());
+    }
+}
--- out/production/Ejercicios-Guia01/README.md
+++ out/production/Ejercicios-Guia01/README.md
@@ -1,1 +1,2 @@
-# Ejercicios-Guia01
+<h1>Ejercicios guía 01</h1>
+<p>En este repositorio se encontrarán los ejercicios de la guía 01 de Programación II</p>
